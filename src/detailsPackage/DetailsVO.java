bfe7d7a4a48838162f61456ef20e69a7522fe713
Sathish <sathishprs@smartglossa>
2016-11-08 09:56:55 +0530

    change class file

--- src/detailsPackage/DetailsVO.java
+++ src/detailsPackage/DetailsVO.java
@@ -1,5 +1,88 @@
 package detailsPackage;
 
+import java.io.InputStream;
+
 public class DetailsVO {
     
+	int userId = 1;
+	String name;
+	String email;
+	String address;
+	String ctz;
+	String gschool;
+	String date;
+	InputStream photo;
+
+	public int getuserId() {
+		return userId;
+	}
+
+	public DetailsVO setuserId(int userId) {
+		this.userId = userId;
+		return this;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public DetailsVO setName(String name) {
+		this.name = name;
+		return this;
+	}
+
+	public String getemail() {
+		return email;
+	}
+
+	public DetailsVO setemail(String email) {
+		this.email = email;
+		return this;
+	}
+
+	public String getaddress() {
+		return address;
+	}
+
+	public DetailsVO setaddress(String address) {
+		this.address = address;
+		return this;
+	}
+
+	public String getctz() {
+		return ctz;
+	}
+
+	public DetailsVO setctz(String ctz) {
+		this.ctz = ctz;
+		return this;
+	}
+
+	public String getgschool() {
+		return gschool;
+	}
+
+	public DetailsVO setgschool(String gschool) {
+		this.gschool = gschool;
+		return this;
+	}
+
+	public String getdate() {
+		return date;
+	}
+
+	public DetailsVO setdate(String date) {
+		this.date = date;
+		return this;
+	}
+
+	public InputStream getphoto() {
+		return photo;
+	}
+
+	public DetailsVO setphoto(InputStream inputStream) {
+		this.photo = inputStream;
+		return this;
+	}
 }
+
